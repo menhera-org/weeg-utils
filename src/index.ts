fe0bdbf742e2dfb93db0eb9653d105c29ac44804
metastable-void <git@yuka.h09.eu>
2023-04-13 18:38:07 +0900

    update dependencies and export VIewRefreshHandler

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,5 @@ export { ExtensibleAttribute } from './ExtensibleAttribute/ExtensibleAttribute';
 export { ExtensibleAttributeDictionary } from './ExtensibleAttribute/ExtensibleAttributeDIctionary';
 export { ExtensibleAttributeProvider } from './ExtensibleAttribute/ExtensibleAttributeProvider';
 export { ExtensibleAttributeSet } from './ExtensibleAttribute/ExtensibleAttributeSet';
+
+export { ViewRefreshHandler } from './ViewRefreshHandler';
